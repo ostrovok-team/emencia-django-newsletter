3104a48acc2a81cdebcbf527d8ee08ff35326cd8
Ilya Batiy <ilya@ostrovok.ru>
2014-02-16 15:58:28 +0400

    * fix unicode

--- emencia/management/commands/send_newsletter.py
+++ emencia/management/commands/send_newsletter.py
@@ -24,7 +24,7 @@ class Command(NoArgsCommand):
             mailer = Mailer(newsletter, verbose=verbose)
             if mailer.can_send:
                 if verbose:
-                    print u'Start emailing %s' % newsletter.title
+                    print u'Start emailing {}'.format(newsletter.title)
                 mailer.run()
 
         if verbose:
